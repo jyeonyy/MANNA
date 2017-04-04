40394ba5ebec0e9a5040c4c968c33e6002ea13a6
김혜민 <hyemin137@gmail.com>
2017-04-04 22:01:52 +0900

    화면 + 카카오로그인 병합

---
+++ app/src/main/java/org/ssutown/manna/LoginActivity.java
@@ -0,0 +1,132 @@
+package org.ssutown.manna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int KAKAO_LOGIN_SUCCESS = 200;
+
+    SessionCallback callback;
+    private static final String TAG = "KakaoLoginActivity";
+    private boolean login = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kakaologin);
+
+        Toast.makeText(getApplicationContext(),"login",Toast.LENGTH_SHORT).show();
+
+        /**카카오톡 로그아웃 요청**/
+        //한번 로그인이 성공하면 세션 정보가 남아있어서 로그인창이 뜨지 않고 바로 onSuccess()메서드를 호출합니다.
+        //테스트 하시기 편하라고 매번 로그아웃 요청을 수행하도록 코드를 넣었습니다 ^^
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                //로그아웃 성공 후 하고싶은 내용 코딩 ~
+            }
+        });
+
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //간편로그인시 호출 ,없으면 간편로그인시 로그인 성공화면으로 넘어가지 않음
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+
+            UserManagement.requestMe(new MeResponseCallback() {
+
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
+                    Logger.d(message);
+
+                    ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                    if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                        finish();
+                    } else {
+                        //redirectMainActivity();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                }
+
+                @Override
+                public void onNotSignedUp() {
+                }
+
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    //로그인에 성공하면 로그인한 사용자의 일련번호, 닉네임, 이미지url등을 리턴합니다.
+                    //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
+                    Log.e("UserProfile", userProfile.toString());
+                    Toast.makeText(getApplicationContext(),"sucess",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+
+            });
+
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.d("myLog","onSessionOpenFailed"+"onSessionOpenFaild");
+
+            if(exception != null){
+                Logger.e(exception);
+            }
+        }
+    }
+}
--- app/src/main/java/org/ssutown/manna/MainActivity.java
+++ app/src/main/java/org/ssutown/manna/MainActivity.java
@@ -1,23 +1,33 @@
 package org.ssutown.manna;
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.app.Fragment;
 
 /**
  * Created by Jiyeon on 2017-03-25.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
 
+        login();
+
     }
+
+    public void login(){
+        Intent loginIntent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivityForResult(loginIntent,0);
+    }
+
+
 
     public void ChangeFragment(View view){
         Fragment fr = new Fragment();
@@ -27,9 +37,13 @@ public class MainActivity extends Activity{
         }else if(view == findViewById(R.id.button1)){
             fr = new MeetingFragment();
 
-        }else if(view == findViewById(R.id.button2)){
+        }else if(view == findViewById(R.id.button2)) {
             fr = new HomeFragment();
 
+        }else if(view == findViewById(R.id.button3)){
+            fr = new ScheduleFragment();
+        } else if (view == findViewById(R.id.button4)) {
+            fr = new SettingFragment();
         }
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
