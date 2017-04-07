649d8f897811cc010ee1c9837dd6b9ab24f84edb
Jiyeon <96_08_01@naver.com>
2017-04-07 11:32:11 +0900

    custom listview, layout

---
+++ app/src/main/java/org/ssutown/manna/ListViewItem.java
@@ -0,0 +1,25 @@
+package org.ssutown.manna;
+
+import android.graphics.drawable.Drawable;
+
+public class ListViewItem {
+    private Drawable iconDrawable ;
+    private String titleStr ;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon ;
+    }
+    public void setTitle(String title) {
+        titleStr = title ;
+    }
+
+
+    public Drawable getIcon() {
+        return this.iconDrawable ;
+    }
+    public String getTitle() {
+        return this.titleStr ;
+    }
+
+}
+
--- app/src/main/java/org/ssutown/manna/MeetingFragment.java
+++ app/src/main/java/org/ssutown/manna/MeetingFragment.java
@@ -1,17 +1,14 @@
 package org.ssutown.manna;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 
 public class MeetingFragment extends Fragment {
@@ -22,32 +19,40 @@ public class MeetingFragment extends Fragment {
         View view = inflater.inflate( R.layout.meeting_fragment, container, false );
 
 
-        Button button = (Button) view.findViewById(R.id.meetingroom);   //selectroom
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent i = new Intent(getActivity(), MeetingActivity.class);
-                FragmentManager fragmentManager = getFragmentManager();
 
-                startActivity(i);
-
-             }
-        });
-
-        ArrayAdapter Adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU);
-
+        ListViewAdapter adapter;
+        adapter = new ListViewAdapter();
         ListView listview = (ListView)view.findViewById(R.id.listview);
-        listview.setAdapter(Adapter);
+        listview.setAdapter(adapter);
+
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.calendar1), "first");
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.home1), "second");
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.person3), "third");
+
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
 
-                // get TextView's Text.
-                String strText = (String) parent.getItemAtPosition(position) ;
+                ListViewItem item = (ListViewItem)parent.getItemAtPosition(position);
+                String titleStr = item.getTitle();
+               // Toast.makeText(getActivity().getApplication(),"gg",Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(getActivity().getApplication(),"gg",Toast.LENGTH_SHORT).show();
+                if(titleStr.equals("first") == true){
+                    Intent i = new Intent(getActivity(), MeetingActivity.class);
+                    startActivity(i);
+
+                }else if(titleStr.equals("second") == true){
+                    Intent i = new Intent(getActivity(), MeetingActivity.class);
+                    startActivity(i);
+
+                }else if(titleStr.equals("third") == true){
+                    Intent i = new Intent(getActivity(), MeetingActivity.class);
+                    startActivity(i);
+
+                }
+
             }
         }) ;
 
