6afe6a694dfe5776bc7dcb64cc272aa02f8b6856
김혜민 <hyemin137@gmail.com>
2017-04-09 18:12:48 +0900

    firebase test

--- app/src/main/java/org/ssutown/manna/LoginActivity.java
+++ app/src/main/java/org/ssutown/manna/LoginActivity.java
@@ -113,6 +113,7 @@ public class LoginActivity extends AppCompatActivity {
                     //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
                     Log.e("UserProfile", userProfile.toString());
                     Toast.makeText(getApplicationContext(),"sucess",Toast.LENGTH_LONG).show();
+                    KakaoProfile profile = new KakaoProfile(userProfile.getId(), userProfile.getNickname(), userProfile.getThumbnailImagePath(), userProfile.getProfileImagePath());
                     finish();
                 }
 
