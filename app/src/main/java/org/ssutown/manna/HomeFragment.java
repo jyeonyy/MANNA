0218fb5aa6a0ac0cc14caebcea1eeeaa4d11f114
Jiyeon <96_08_01@naver.com>
2017-04-11 14:43:44 +0900

    카카오톡 프로필 정보 레이아웃

--- app/src/main/java/org/ssutown/manna/HomeFragment.java
+++ app/src/main/java/org/ssutown/manna/HomeFragment.java
@@ -8,8 +8,16 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+  //  private ImageView kakaoprofile;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+      //  kakaoprofile = (ImageView)getView().findViewById(R.id.circle);
+       // kakaoprofile.setBackground(new ShapeDrawable(new OvalShape()));
+       // kakaoprofile.setClipToOutline(true);
         return inflater.inflate(R.layout.home_fragment, container, false);
+
+
     }
 }
