6a69a5e5f1350719847f93e7adc863cc573a83e6
Yun-na-hae <pqlove5@hanmail.net>
2017-05-18 19:50:21 +0900

    dateformat/timeformat

--- app/src/main/java/org/ssutown/manna/AddAppointActivity.java
+++ app/src/main/java/org/ssutown/manna/AddAppointActivity.java
@@ -1,9 +1,19 @@
 package org.ssutown.manna;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import org.ssutown.manna.GoogleCalendar.GoogleCalendarActivity;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by YNH on 2017. 5. 7..
@@ -19,10 +29,61 @@ public class AddAppointActivity extends Activity {
 
         View.OnClickListener listener = new View.OnClickListener()
         {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v)
             {
                 //ADD Button 클릭시 이벤트 추가
+//                GoogleCalendarActivity google_cal = new GoogleCalendarActivity();
+//                Bundle name = new Bundle();
+//                name.putChar("NameOfAppoint",getText(name));
+//                google_cal.setArguments(name);
+
+                SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
+
+                final EditText name = (EditText)findViewById(R.id.AppointName);
+
+                final DatePicker startDatePicker = (DatePicker)findViewById(R.id.datePicker_start);
+                final DatePicker endDatePicker = (DatePicker)findViewById(R.id.datePicker_end);
+                final TimePicker startTimePicker = (TimePicker)findViewById(R.id.timePicker_start);
+                final TimePicker endTimePicker = (TimePicker)findViewById(R.id.timePicker_end);
+
+                String startDate = dateformat.format
+                        (new java.sql.Date(startDatePicker.getYear()-1900, startDatePicker.getMonth(), startDatePicker.getDayOfMonth()));
+                String endDate = dateformat.format
+                        (new java.sql.Date(endDatePicker.getYear()-1900, endDatePicker.getMonth(), endDatePicker.getDayOfMonth()));
+                String startTime = timeformat.format
+                        (new java.sql.Time(startTimePicker.getHour(), startTimePicker.getMinute(),0));
+                String endTime = timeformat.format
+                        (new java.sql.Time(endTimePicker.getHour(), endTimePicker.getMinute(),0));
+
+                String start = new StringBuilder()
+                        .append(startDate)
+                        .append('T')
+                        .append(startTime)
+                        .append("+09:00")
+                        .toString();
+                String end = new StringBuilder()
+                        .append(endDate)
+                        .append('T')
+                        .append(endTime)
+                        .append("+09:00")
+                        .toString();
+
+                Intent intent = new Intent(getApplicationContext(), GoogleCalendarActivity.class);
+
+                intent.putExtra("NameOfAppoint",name.getText().toString());
+                intent.putExtra("Start",start);
+                intent.putExtra("End",end);
+
+
+//                Toast toast = Toast.makeText(getApplicationContext(),start,Toast.LENGTH_SHORT);
+//                toast.show();
+
+
+
+
 
             }
         };
@@ -31,5 +92,11 @@ public class AddAppointActivity extends Activity {
         addBtn.setOnClickListener(listener);
 
     }
+
+//    protected int getHour(int baseLine,int hour)
+//    {
+//        if(baseLine == 1900)
+//            return 1;
+//    }
 
 }
