f549109e453dee5c3c80b7b5f954f494ff216846
윤나혜 <pqlove0256@gmail.com>
2017-05-30 02:46:19 +0900

    각 미팅 방 캘린더와 전체미팅 캘린더 추가

---
+++ app/src/main/java/org/ssutown/manna/MeetingSchedule/MeetingCalendarAdapter.java
@@ -0,0 +1,206 @@
+package org.ssutown.manna.MeetingSchedule;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.ssutown.manna.R;
+
+/**
+ * Created by YNH on 2017. 5. 30..
+ */
+
+public class MeetingCalendarAdapter extends BaseAdapter {
+    // Variables
+    private Context mContext;
+    private static MeetingCalendarAdapter.ViewHolder mHolder;
+    int mWeekDayNames = 7;
+    int mGridViewIndexOffset = 1;
+
+    private static class ViewHolder {
+        ImageView mSelectedDayImageView;
+        TextView mTextView;
+        ImageView mSavedEventImageView;
+    }
+
+    // Constructor
+    public MeetingCalendarAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        if (MeetingCalendar.mFirstDay != -1 && MeetingCalendar.mNumDaysInMonth != -1) {
+            Log.d("GRID_COUNT", String.valueOf(mWeekDayNames + MeetingCalendar.mFirstDay + MeetingCalendar.mNumDaysInMonth));
+            return mWeekDayNames + MeetingCalendar.mFirstDay + MeetingCalendar.mNumDaysInMonth;
+        }
+
+        return mWeekDayNames;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.layout_material_day, parent, false);
+
+            mHolder = new MeetingCalendarAdapter.ViewHolder();
+
+            if (convertView != null) {
+                mHolder.mSelectedDayImageView = (ImageView) convertView.findViewById(R.id.material_calendar_selected_day);
+                mHolder.mTextView = (TextView) convertView.findViewById(R.id.material_calendar_day);
+                mHolder.mSavedEventImageView = (ImageView) convertView.findViewById(R.id.saved_event_imageView);
+                convertView.setTag(mHolder);
+            }
+        } else {
+            mHolder = (MeetingCalendarAdapter.ViewHolder) convertView.getTag();
+        }
+
+        if (mHolder.mSelectedDayImageView != null) {
+            GridView gridView = (GridView) parent;
+            Log.d("ITEM_CHECKED_POSITION", String.valueOf(gridView.isItemChecked(position)));
+            if (gridView.isItemChecked(position)) {
+                Animation feedBackAnimation = AnimationUtils.loadAnimation(mContext, R.anim.selected_day_feedback);
+                mHolder.mSelectedDayImageView.setVisibility(View.VISIBLE);
+
+                if (feedBackAnimation != null) {
+                    mHolder.mSelectedDayImageView.startAnimation(feedBackAnimation);
+                }
+            } else {
+                mHolder.mSelectedDayImageView.setVisibility(View.INVISIBLE);
+            }
+        }
+
+
+        if (mHolder.mTextView != null) {
+            setCalendarDay(position);
+        }
+
+        if (mHolder.mSavedEventImageView != null) {
+            setSavedEvent(position);
+        }
+
+        return convertView;
+    }
+
+
+
+    private void setCalendarDay(int position) {
+        if (position <= mWeekDayNames - mGridViewIndexOffset + MeetingCalendar.mFirstDay) {
+            mHolder.mTextView.setTextColor(mContext.getResources().getColor(R.color.calendar_day_text_color));
+            Log.d("NO_CLICK_POSITION", String.valueOf(position));
+        } else {
+            mHolder.mTextView.setTextColor(mContext.getResources().getColor(R.color.calendar_number_text_color));
+        }
+
+        switch (position) {
+            case 0:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.sunday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 1:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.monday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 2:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.tuesday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 3:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.wednesday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 4:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.thursday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 5:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.friday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            case 6:
+                mHolder.mTextView.setText(mContext.getResources().getString(R.string.saturday));
+                mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                break;
+
+            default:
+                Log.d("CURRENT_POSITION", String.valueOf(position));
+                if (position < mWeekDayNames + MeetingCalendar.mFirstDay) {
+                    Log.d("BLANK_POSITION", "This is a blank day");
+                    mHolder.mTextView.setText("");
+                    mHolder.mTextView.setTypeface(Typeface.DEFAULT);
+                } else {
+                    mHolder.mTextView.setText(String.valueOf(position - (mWeekDayNames - mGridViewIndexOffset) -
+                            MeetingCalendar.mFirstDay));
+                    mHolder.mTextView.setTypeface(Typeface.DEFAULT_BOLD);
+
+                    if (MeetingCalendar.mCurrentDay != -1) {
+                        int startingPosition = mWeekDayNames - mGridViewIndexOffset + MeetingCalendar.mFirstDay;
+                        int currentDayPosition = startingPosition + MeetingCalendar.mCurrentDay;
+
+                        if (position == currentDayPosition) {
+                            mHolder.mTextView.setTextColor(mContext.getResources().getColor(
+                                    R.color.calendar_current_number_text_color));
+                        } else {
+                            mHolder.mTextView.setTextColor(mContext.getResources().getColor(
+                                    R.color.calendar_number_text_color));
+                        }
+                    } else {
+                        mHolder.mTextView.setTextColor(mContext.getResources().getColor(
+                                R.color.calendar_number_text_color));
+                    }
+                }
+                break;
+        }
+    }
+
+    private void setSavedEvent(int position) {
+        // Reset saved position indicator
+        mHolder.mSavedEventImageView.setVisibility(View.INVISIBLE);
+
+        if (MeetingCalendar.mFirstDay != -1 && MeetingCalendarFragment.mSavedEventDays != null &&
+                MeetingCalendarFragment.mSavedEventDays.size() > 0) {
+
+            int startingPosition = mWeekDayNames - mGridViewIndexOffset + MeetingCalendar.mFirstDay;
+//            Log.d("SAVED_EVENT_STARTING_POS", String.valueOf(startingPosition));
+            if (position > startingPosition) {
+                for (int i = 0; i < MeetingCalendarFragment.mSavedEventDays.size(); i++) {
+                    int savedEventPosition = startingPosition + MeetingCalendarFragment.mSavedEventDays.get(i);
+
+                    Log.d("POSITION", String.valueOf(position));
+                    Log.d("SAVED_POSITION", String.valueOf(savedEventPosition));
+                    if (position == savedEventPosition) {
+                        mHolder.mSavedEventImageView.setVisibility(View.VISIBLE);
+                    }
+                }
+            } else {
+                mHolder.mSavedEventImageView.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+}
--- app/src/main/java/org/ssutown/manna/ScheduleFragment.java
+++ app/src/main/java/org/ssutown/manna/ScheduleFragment.java
@@ -1,39 +1,33 @@
 package org.ssutown.manna;
 
 
-import android.util.Log;
-import android.view.View;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
 
-import org.ssutown.manna.meeting.*;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
-
-import static android.content.ContentValues.TAG;
+import org.ssutown.manna.MeetingSchedule.MeetingCalendarFragment;
 
 public class ScheduleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-        return inflater.inflate(R.layout.schedule_fragment, container, false);
+        super.onCreate(savedInstanceState);
 
+        View view = inflater.inflate( R.layout.schedule_fragment, container, false );
+        Log.i("i'm personfragment","layout_main");
 
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.main_container, new MeetingCalendarFragment()).commit();
+            Log.i("savedinstancestate null","main_container");
+        }
 
-
-
+        return view;
 
     }
+
+
 }
